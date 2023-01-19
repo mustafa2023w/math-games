05dfab2f458972a046a4c683a32ee340dd218627
mustafa2023w <121315397+mustafa2023w@users.noreply.github.com>
2023-01-19 19:03:58 +0500

    Add files via upload

---
+++ AddScript.js
@@ -0,0 +1,72 @@
+ 
+function generateProblem(){                             
+    var a = Math.floor((Math.random() * 15) + 1); 
+    var b = Math.floor((Math.random() * 15) + 1);
+    var op = '+'
+    var problem  = a + " " + op + " " + b 
+    document.getElementById("problem").innerHTML = problem 
+}
+
+
+ 
+var score =0;   var q=1;           
+
+function bad_score(){
+  document.getElementById("score").innerHTML = score;
+  if (score > 2) {
+    score-=2;
+  }
+  else {
+    score=0;
+  }
+  document.getElementById("score").innerHTML = score;
+}
+
+function good_score(){
+  document.getElementById("score").innerHTML = score;
+  score=score+5;  
+  document.getElementById("score").innerHTML = score;
+}
+
+
+
+ 
+function checkAnswer(){
+
+    var problem= document.getElementById("problem").innerHTML;
+    var correctAnswer=eval(problem);
+    var userAnswer=document.getElementById("answers").value ;
+    
+    if (userAnswer == correctAnswer){
+      
+     
+      good_score(); 
+       
+      q=q+1;  document.getElementById("q").innerHTML = q; 
+
+      generateProblem();
+      answers.value=''
+    }
+    else{
+      alert("Wrong Answer 🙁");
+      answers.value=''
+       
+      bad_score();  
+      
+
+      
+  
+  }
+ 
+}
+
+
+
+window.addEventListener("load", function(){
+  generateProblem();
+})
+
+ 
+
+
+  
---
+++ DivScript.js
@@ -0,0 +1,68 @@
+
+function generateProblem(){
+  var a = Math.floor((Math.random() * 25) + 1);  
+  var b = Math.floor((Math.random() * 10) + 1);
+  var op = '/'
+  var problem  = a + " " + op + " " + b 
+  document.getElementById("problem").innerHTML = problem 
+
+  document.getElementById('a').innerHTML=a; 
+  document.getElementById('b').innerHTML=b; 
+  
+}
+
+
+var score =0;   var q=1;
+
+function bad_score(){
+  document.getElementById("score").innerHTML = score;
+  if (score > 2) {
+    score-=2;
+  }
+  else {
+    score=0;
+  }
+  document.getElementById("score").innerHTML = score;
+}
+
+function good_score(){
+  document.getElementById("score").innerHTML = score;
+  score=score+5;  
+  document.getElementById("score").innerHTML = score;
+}
+
+
+function checkAnswer(){
+
+  var problem= document.getElementById("problem").innerHTML;
+  var correctAnswer=Math.trunc(eval(problem));  
+  var userAnswer=document.getElementById("answers").value ;
+  
+  if (userAnswer == correctAnswer){
+    
+
+    good_score();  
+      
+    q=q+1;  document.getElementById("q").innerHTML = q;  
+
+    generateProblem();
+    answers.value=''
+  }
+  else{
+    alert("Wrong Answer 🙁");
+    answers.value=''
+    bad_score();   
+}
+
+}
+
+
+
+window.addEventListener("load", function(){
+generateProblem();
+})
+
+
+
+
+
